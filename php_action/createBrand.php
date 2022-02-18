37de18ebdb78ec7851ca9a5cacfbcd7f7432726e
abhijarvis <77891952+abhijarvis@users.noreply.github.com>
2022-02-18 19:10:41 +0530

    Add files via upload

---
+++ php_action/createBrand.php
@@ -0,0 +1,23 @@
+<?php 	
+
+require_once 'core.php';
+
+$valid['success'] = array('success' => false, 'messages' => array());
+
+if($_POST) {	
+
+	$brandName = $_POST['brandName'];
+	$brandStatus = $_POST['brandStatus'];
+	$sql = "INSERT INTO brands (brand_name, brand_active, brand_status) VALUES ('$brandName','$brandStatus', 1)";
+
+	if($connect->query($sql) === TRUE) {
+		header('location: http://localhost/inventory/brand.php');	
+	} else {
+	 	$valid['success'] = false;
+	 	$valid['messages'] = "Error while adding the brand.";
+	}
+	$connect->close();
+
+	echo json_encode($valid);
+ 
+}
